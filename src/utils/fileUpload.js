bd4c7eedbc9c85a8bc385d9ac96c9802c1fc3e3b
Yourekaa Engineering <deedeeowoeye@gmail.com>
2024-11-04 13:31:52 +0100

    chore: increase the maximum filesize

--- src/utils/fileUpload.js
+++ src/utils/fileUpload.js
@@ -19,7 +19,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 1024 * 1024 * 3 }
+  limits: { fileSize: 1024 * 1024 * 6 }
 });
 
 module.exports = { upload };
