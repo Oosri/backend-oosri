401dd9f26b5cd34f152504a5f21d0097b7cafdc4
Hassan Oladipupo <109126045+Hassan-oladipupo@users.noreply.github.com>
2025-06-22 12:10:37 +0100

    Update buyerProductService.js

--- src/Buyer/Service/buyerProductService.js
+++ src/Buyer/Service/buyerProductService.js
@@ -57,14 +57,9 @@ const index = client.initIndex(process.env.ALGOLIA_INDEX_NAME);
             ? `${sellerDetails.firstName} ${sellerDetails.lastName}`
             : 'Unknown Seller';
 
-     const productReviews = await buyerProductReview.find({ productId: product._id });
-
-      let productRating = 0;
-    if (productReviews.length > 0) {
-    const totalRating = productReviews.reduce((sum, review) => sum + review.rating, 0);
-     productRating = totalRating / productReviews.length;
-   }
-
+        const productReview = await buyerProductReview.findOne({ productId: product._id });
+              
+        const productRating = productReview ? productReview.productRating : 0;
   
           return {
             _id: product._id,
