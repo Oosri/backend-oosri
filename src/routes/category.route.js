3e962f1bdf5dc99aa38fa21945d8febac73afc22
Deedee <deedeeowoeye@gmail.com>
2025-06-25 20:21:14 +0100

    chore: upload category image

--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -5,16 +5,21 @@ const {
   getCategory,
   deleteCategory,
   createSubcategory,
-  getSubcategories
+  getSubcategories,
+  updateCategory,
+  updateSubcategory
 } = require('../controllers/categoryController');
+const upload = require('../Buyer/middlewares/fileUploadMiddleware');
 
 const router = express.Router();
 
-router.post('/', createCategory);
+router.post('/', upload.single('image'), createCategory);
 router.get('/', getCategories);
 router.get('/:id', getCategory);
 router.delete('/:id', deleteCategory);
 router.post('/subcategory', createSubcategory);
 router.get('/:categoryId/subcategory', getSubcategories);
+router.put('/:id', upload.single('image'), updateCategory);
+router.put('/:categoryId/subcategory/:id', updateSubcategory);
 
 module.exports = router;
