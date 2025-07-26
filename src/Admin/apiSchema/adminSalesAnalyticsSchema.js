e9925820e1aaebb98f425592efe0302c6ee6d275
Hassan Oladipupo <109126045+Hassan-oladipupo@users.noreply.github.com>
2025-07-26 18:25:42 +0100

    new code commit

--- src/Admin/apiSchema/adminSalesAnalyticsSchema.js
+++ src/Admin/apiSchema/adminSalesAnalyticsSchema.js
@@ -7,3 +7,7 @@ module.exports.retrieveProductAnalyticsSchema = Joi.object().keys({
   });
 
   
+module.exports.retrieveTopProductsAnalyticsSchema = Joi.object().keys({
+    category: Joi.string().optional(),
+    dateFilter: Joi.string().valid('thisWeek', 'lastWeek', 'thisMonth', 'lastMonth', 'thisYear', 'lastYear').optional(),
+  });
